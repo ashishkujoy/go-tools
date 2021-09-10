24835f493c4948fea30de9434f47e75893410465
ashishku <ashishku@thoughtworks.com>
2021-09-10 18:32:37 +0530

    A cp command

---
+++ cp.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	source := os.Args[1]
+	destination := os.Args[2]
+
+
+	err := filepath.Walk(source, walkFunc(source, destination))
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func walkFunc(source, destination string) filepath.WalkFunc {
+	return func(path string, stat os.FileInfo, err error) error {
+		dest := newPath(source, destination, path)
+		fileInfo, err := os.Stat(path)
+		if err != nil {
+			return nil
+		}
+
+		if fileInfo.IsDir() {
+			return os.MkdirAll(dest, 0777)
+		} else {
+			return copy(path, dest, 1024)
+		}
+	}
+}
+
+func newPath(source, destination, currentPath string) string {
+	return strings.Replace(currentPath, source, destination, 1)
+}
+
+func copy(source, destination string, bufferSize int) error {
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(destination)
+
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, bufferSize)
+
+	for {
+		numberOfBytes, err := sourceFile.Read(buf)
+
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if numberOfBytes == 0 {
+			break
+		}
+
+		if _, err = destinationFile.Write(buf[:numberOfBytes]); err != nil {
+			return nil
+		}
+	}
+	return err
+}
